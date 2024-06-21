24827810b90f34c8d3b5c4f4395fa16f9ed1a0fd
sean <seanjiangsh@gmail.com>
2024-06-21 08:26:45 +0800

    fix: modified decoder for old encoding patient names

--- src/utils/dcm/decoder.ts
+++ src/utils/dcm/decoder.ts
@@ -1,22 +1,42 @@
 import { DataSet, Element } from "dicom-parser";
 
 const DECODE_LABEL: { [key: string]: string } = {
-  "ISO_IR 101": "iso-8859-2",
-  "ISO_IR 109": "iso-8859-3",
-  "ISO_IR 110": "iso-8859-4",
-  "ISO_IR 144": "iso-8859-5",
-  "ISO_IR 127": "iso-8859-6",
-  "ISO_IR 126": "iso-8859-7",
-  "ISO_IR 138": "iso-8859-8",
-  "ISO_IR 148": "iso-8859-9",
-  "ISO_IR 166": "iso-8859-11",
-  "ISO 2022 IR 87": "iso-2022-jp",
+  "ISO_IR 6": "iso-8859-1", // Latin alphabet No. 1 (Western European languages)
+  "ISO_IR 100": "iso-8859-1", // Latin alphabet No. 1 (Western European languages)
+  "ISO_IR 101": "iso-8859-2", // Latin alphabet No. 2 (Eastern European languages)
+  "ISO_IR 109": "iso-8859-3", // Latin alphabet No. 3 (South European languages)
+  "ISO_IR 110": "iso-8859-4", // Latin alphabet No. 4 (North European languages)
+  "ISO_IR 126": "iso-8859-7", // Greek
+  "ISO_IR 127": "iso-8859-6", // Arabic
+  "ISO_IR 138": "iso-8859-8", // Hebrew
+  "ISO_IR 144": "iso-8859-5", // Cyrillic alphabet (Eastern European languages, including Russian)
+  "ISO_IR 148": "iso-8859-9", // Turkish
+  "ISO_IR 166": "iso-8859-11", // Thai
+  "ISO_IR 192": "utf-8",
+  "ISO 2022 IR 6": "iso-8859-1",
+  "ISO 2022 IR 100": "iso-8859-1",
+  "ISO 2022 IR 101": "iso-8859-2",
+  "ISO 2022 IR 109": "iso-8859-3",
+  "ISO 2022 IR 110": "iso-8859-4",
+  "ISO 2022 IR 126": "iso-8859-7",
+  "ISO 2022 IR 127": "iso-8859-6",
+  "ISO 2022 IR 138": "iso-8859-8",
+  "ISO 2022 IR 144": "iso-8859-5",
+  "ISO 2022 IR 148": "iso-8859-9",
+  "ISO 2022 IR 166": "iso-8859-11",
+  // Japanese
   "ISO_IR 13": "shift-jis",
   "ISO 2022 IR 13": "shift-jis",
-  "ISO_IR 192": "utf-8",
+  "ISO 2022 IR 87": "iso-2022-jp",
+  "ISO 2022 IR 159": "iso-2022-jp",
+  // Korean
+  "ISO 2022 IR 149": "euc-kr",
+  // Simplified Chinese
+  "ISO 2022 58": "gb2312",
+  "ISO 2022 IR 58": "gb2312",
+  "ISO 2022 GBK": "gbk",
   GB18030: "gb18030",
-  GB2312: "gb2312",
-  GBK: "chinese",
+  GBK: "gbk",
 };
 
 const getLangLabel = () => {
@@ -33,17 +53,17 @@ const getLangLabel = () => {
   }
 };
 
-const splitPN = (buffer: Uint8Array) => {
-  let tmp = [];
-  let result = [];
+const splitPN = (buffer: Uint8Array): Array<Uint8Array> => {
+  let tmp: Array<number> = [];
+  let result: Array<Uint8Array> = [];
   const { length } = buffer;
   for (let i = 0; i < length; i++) {
     if (buffer[i] === 0x3d) {
-      result.push(tmp);
+      result.push(new Uint8Array(tmp));
       tmp = [];
     } else if (i === length - 1) {
       tmp.push(buffer[i]);
-      result.push(tmp);
+      result.push(new Uint8Array(tmp));
     } else {
       tmp.push(buffer[i]);
     }
@@ -63,65 +83,83 @@ const cleanString = (str: string) => {
   return str;
 };
 
+const toHexString = (buffer: Uint8Array) =>
+  Array.from(buffer)
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join(" ");
+
+const clearStringEscape = (str: string) => {
+  const buffer = new TextEncoder().encode(str);
+  console.log("clearStringEscape", toHexString(buffer));
+  const { length } = buffer;
+  let tmp: Array<number> = [];
+  for (let i = 0; i < length; i++) {
+    if (buffer[i] === 0x1b) {
+      i += 2;
+    } else {
+      tmp.push(buffer[i]);
+    }
+  }
+  return new TextDecoder("utf-8").decode(new Uint8Array(tmp));
+};
+
+const isGarbled = (str: string) => str.includes("�");
+
+const checkGarbledAndTryDecode = (
+  decoded: string,
+  buffer: Uint8Array,
+  charsets: Array<string>
+) => {
+  if (!isGarbled(decoded)) return decoded;
+  for (const charset of charsets) {
+    const utfLabel = DECODE_LABEL[charset] || getLangLabel();
+    const textDecoder = new TextDecoder(utfLabel);
+    const decoded = textDecoder.decode(buffer);
+    if (!isGarbled(decoded)) return decoded;
+  }
+  return decoded;
+};
+
 export const valueDecode = (dataset: DataSet, dcmElem: Element, vr: string) => {
   const buffer = dataset.byteArray;
   const charset = dataset.string("x00080005") || "ISO_IR 192";
-  const charsets = charset.split("\\");
+  const charsets = charset.split("\\").map((c) => c || "ISO_IR 192");
   const valueBuffer = buffer.subarray(
     dcmElem.dataOffset,
     dcmElem.dataOffset + dcmElem.length
   );
 
   // * 0x3d or "=" symbol is the separator of VR=PN value, will needs to decode by charset sequence
-  const valueBuffArray =
+  const valueBuffArray: Array<Uint8Array> =
     vr === "PN" && valueBuffer.includes(0x3d)
       ? splitPN(valueBuffer)
       : [valueBuffer];
 
   // if (vr === "PN") {
-  //   console.log(
-  //     Array.from(valueBuffer)
-  //       .map((b) => b.toString(16).padStart(2, "0"))
-  //       .join(" ")
-  //   );
+  //   console.log(toHexString(valueBuffer));
   //   console.log({
   //     tag: dcmElem.tag,
   //     charsets,
-  //     valueLength: valueBuffArray.length,
-  //     valueBuffArray: valueBuffArray.map((v) =>
-  //       Array.from(v)
-  //         .map((b) => b.toString(16).padStart(2, "0"))
-  //         .join(" ")
-  //     ),
+  //     valueBuffArray: valueBuffArray.map(toHexString),
   //   });
   // }
 
   const values = valueBuffArray.reduce<Array<string>>((p, c, idx) => {
-    let term = charsets[idx];
-    if (charset === "ISO_IR 192" || !term) {
-      term = "ISO_IR 192";
-    }
+    const term = charsets[idx] || charsets.at(-1) || "ISO_IR 192";
 
+    // let term = charsets[idx] || "ISO_IR 192";
     // if (idx === 0) term = "ISO_IR 192";
     // else if (idx === 1) term = "ISO 2022 IR 87";
-    // else if (idx === 2) term = "ISO 2022 IR 87";
+    // else if (idx === 2) term = "ISO 2022 IR 13";
 
-    const utfLabel = DECODE_LABEL[term];
-    const textDecoder = utfLabel
-      ? new TextDecoder(utfLabel)
-      : new TextDecoder(getLangLabel());
+    const utfLabel = DECODE_LABEL[term] || getLangLabel();
 
+    const textDecoder = new TextDecoder(utfLabel);
     const decoded = textDecoder.decode(new Uint8Array(c));
-    // if (vr === "PN") {
-    //   console.log({
-    //     idx,
-    //     term,
-    //     utfLabel,
-    //     encoding: textDecoder.encoding,
-    //     decoded,
-    //   });
-    // }
-    const result = cleanString(decoded.trim());
+    const checked = checkGarbledAndTryDecode(decoded, c, charsets);
+    const cleared = clearStringEscape(checked);
+
+    const result = cleanString(cleared.trim());
     if (!result.length) return p;
 
     const multiValues = result.split("\\");
