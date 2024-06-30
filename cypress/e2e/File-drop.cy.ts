68cc687ddce1ef9722ecc634d206d6e64ac1727d
sean <seanjiangsh@gmail.com>
2024-07-01 06:55:08 +0800

    test: added more File-drop element tests

--- cypress/e2e/File-drop.cy.ts
+++ cypress/e2e/File-drop.cy.ts
@@ -11,9 +11,21 @@ describe("File drop e2e tests", () => {
         const zIndex = $el.css("z-index");
         expect(zIndex).to.eq("10");
       });
+
+    cy.get('[id="FileDrop"]').contains("Drop the DICOM file here");
+    cy.get('[id="FileDrop"]').contains("or");
+    cy.get('[id="FileDrop"]').contains("Load a sample file");
+
+    cy.get('[id="loadDefaultButton-MR"]').should("exist").contains("MRI (MR)");
+    cy.get('[id="loadDefaultButton-DX"]')
+      .should("exist")
+      .contains("X-ray (DX)");
+    cy.get('[id="loadDefaultButton-SR"]')
+      .should("exist")
+      .contains("Structured Report (SR)");
   });
 
-  it("renders proper FileDrop elements after file dropped", () => {
+  it("renders proper elements after file dropped", () => {
     cy.fixture("MR.dcm", "binary").then((fileContent) => {
       cy.get('[id="FileDrop"]').attachFile(
         {
@@ -25,14 +37,25 @@ describe("File drop e2e tests", () => {
         { subjectType: "drag-n-drop" }
       );
       cy.wait(1000);
-
-      // * file drop element should be in the background before file dropped
-      cy.get('[id="FileDrop"]').then(($el) => {
-        const zIndex = $el.css("z-index");
-        expect(zIndex).to.eq("-10");
-      });
-
-      cy.get('[id="TagTable"]').should("exist");
     });
+
+    // * file drop element should be in the background before file dropped
+    cy.get('[id="FileDrop"]').then(($el) => {
+      const zIndex = $el.css("z-index");
+      expect(zIndex).to.eq("-10");
+    });
+
+    cy.get('[id="TagTable"]').should("exist");
+  });
+
+  it("render proper elements after DX sample file loaded", () => {
+    cy.get('[id="loadDefaultButton-DX"]').click();
+    cy.wait(1000);
+
+    cy.get('[id="FileDrop"]').then(($el) => {
+      const zIndex = $el.css("z-index");
+      expect(zIndex).to.eq("-10");
+    });
+    cy.get('[id="TagTable"]').should("exist");
   });
 });
