f509830ee426e80728e2084d54a89aa7f00fd5c3
sean <seanjiangsh@gmail.com>
2024-08-31 06:50:16 +0800

    feat: added CT overlay image display visual regression test

--- cypress/e2e/Image-view.cy.ts
+++ cypress/e2e/Image-view.cy.ts
@@ -2,6 +2,7 @@ describe("Image view", () => {
   beforeEach(() => {
     cy.visit("/");
   });
+
   it("should display MR image properly", () => {
     // Load a sample file first
     cy.get("#loadDefaultButton-MR").click();
@@ -18,5 +19,33 @@ describe("Image view", () => {
     // Check the image display
     cy.wait(3000);
     cy.compareSnapshot("MR-image-display");
+  });
+
+  it("should display CT overlay image properly", () => {
+    // Load a sample file first
+    cy.fixture("CT-overlay.dcm", "binary").then((fileContent) => {
+      cy.get("#FileDrop").attachFile(
+        {
+          fileContent,
+          fileName: "CT-overlay.dcm",
+          mimeType: "application/dicom",
+          encoding: "binary",
+        },
+        { subjectType: "drag-n-drop" }
+      );
+      cy.wait(1000);
+    });
+
+    // Open the drawer
+    cy.get("#Appbar-menu-button").click();
+    cy.get("#Drawer").should("be.visible");
+
+    // Click the "View Image" button
+    cy.get("#Image-view-dialog-button").click();
+    cy.get("#Image-view-dialog").should("be.visible");
+
+    // Check the image display
+    cy.wait(3000);
+    cy.compareSnapshot("CT-overlay-display");
   });
 });
