35f9229ed0569cad2e60dc207f8f65befe2dec0f
sean <seanjiangsh@gmail.com>
2024-09-08 08:52:33 +0800

    fix: package update to resolve node v22.8.0 Cypress config issue

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,35 +8,6 @@ const setupNodeEvents = (
 ) => {
   cypressCoverageTask(on, config);
   configureVisualRegression(on);
-
-  on("before:browser:launch", (browser, launchOptions) => {
-    console.log(
-      "launching browser %s is headless? %s",
-      browser.name,
-      browser.isHeadless
-    );
-
-    // the browser width and height we want to get
-    // our screenshots and videos will be of that resolution
-    const width = 1000;
-    const height = 660;
-
-    console.log("setting the browser window size to %d x %d", width, height);
-
-    if (browser.name === "chrome") {
-      launchOptions.args.push(`--window-size=${width},${height}`);
-
-      // force screen to be non-retina and just use our given resolution
-      launchOptions.args.push("--force-device-scale-factor=1");
-    }
-
-    if (browser.name === "electron") {
-      launchOptions.preferences.width = width;
-      launchOptions.preferences.height = height;
-    }
-    return launchOptions;
-  });
-
   return config;
 };
 
